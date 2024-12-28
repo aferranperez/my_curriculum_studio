d5ab6266865e638b54dcf928dcb5470c31a501cc
Ale_Ferran98 <aferranperez@gmail.com>
2024-12-28 18:36:28 +0000

    add: modification in FeedCard.vue to use it in Markdown

--- components/content/FeedCard.vue
+++ components/content/FeedCard.vue
@@ -2,7 +2,7 @@
     <article
         class="flex flex-col gap-y-2 p-4 rounded-2xl"
         :class="theme ? 'hover:bg-gray-800' : 'hover:bg-slate-100'">
-            <div v-if="true" class="flex items-center py-5">
+            <div v-if="is_pinned" class="flex items-center py-5">
                 <div class="flex-initial w-5">
                     <Icon
                         name="mdi:pin"
@@ -15,22 +15,21 @@
             <div class="flex flex-col lg:flex-row gap-x-4">
                 <div class="avatar placeholder h-12 mb-4">
                     <div class="bg-neutral text-neutral-content rounded-full w-12">
-                        <NuxtImg src="https://scdnkijrtsbaxifoqytd.supabase.co/storage/v1/object/public/posts/profile_image.jpg" loading="lazy"/>
+                        <NuxtImg src="profile_image.jpg" loading="lazy"/>
                     </div>
                 </div>
                 <div class="flex flex-col gap-y-4">
-                    <div>
+                    <div class="flex">
                         <a href="" class="font-bold">Ale ✨</a>
-                        <span> • </span>
-                        <time class="text-fluid--2"> 
+                        <time class="font-bold text-fluid--2"> 
                             <slot name="created_at"/>
                         </time>
                     </div>
-                    <!-- <div class="flex flex-row flex-wrap gap-1">
-                        <div v-for="badge in post.badges"  class="badge badge-primary badge-md">
-                            <span class="font-semibold">{{badge.title}}</span>
+                    <div class="flex flex-row flex-wrap gap-1">
+                        <div v-for="badge in badges"  class="badge badge-primary badge-md">
+                            <span class="font-semibold">{{badge}}</span>
                         </div>
-                    </div> -->
+                    </div>
                     <!-- <NuxtLink :to="`/feed/${post.slug}`"> -->
                         <h2 class="font-bold text-3xl" :class="theme ? 'text-white' : 'text-black'">
                             <slot name="title"/>
@@ -39,9 +38,10 @@
                     <p class="mb-4">
                         <slot name="description"/>
                     </p>
-                    <!-- <picture>
-                        <NuxtImg class="rounded-xl" width="800px" height="500px" :src="post.image_url" loading="lazy"/>
-                    </picture> -->
+                    <picture>
+                        <NuxtImg class="rounded-xl" width="800px" height="500px" :src="src" loading="lazy"/>
+                    </picture>
+                    <div></div>
                     <ShareBox/>
                 </div>
             </div>
@@ -50,4 +50,10 @@
 
 <script setup lang="ts">
     const theme = useState('theme');
+    
+    const { src, badges, is_pinned } = defineProps<{
+        src: string,
+        badges: [],
+        is_pinned: boolean
+    }>()
 </script>
--- content/feed.md
+++ content/feed.md
@@ -1,12 +1,9 @@
 ::feed-card
-#created_at
-2024-12-12
-#title
-Development of Facial Recognition Algorithms with OpenCV and Python
-#description
-We present a project that uses a Raspberry Pi to perform real-time face detection and recognition, and sends them to a server written in Django via the MQTT protocol. On the server, facial recognition models are trained with the OpenCV library and sent back to the Raspberry Pi. The objective of this project is to demonstrate the feasibility and potential of using low-cost, high-performance devices to implement facial recognition systems.
-::
-::feed-card
+---
+src: post_opencv.jpg
+badges: ['Facial Recognition', 'Internet of Things', 'Python Developer', 'IoT', 'OpenCV', 'MQTT']
+
+---
 #created_at
 2024-12-12
 #title
